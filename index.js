2f8091fe6ccbe28b703cba86f6f5c0987fbd7b9d
Alejandro <alejelias@gmail.com>
2022-11-22 10:00:17 -0500

    commit tree 7a27e0930e082e45bf475d9e94df9ceac37de459

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numBlocks){
+     console.log (42 - numBlocks);
+     return 42 - numBlocks;
+}
+function  distanceFromHqInFeet(distance){
+    console.log(distanceFromHqInBlocks(numBlocks) * distance);
+    return (distanceFromHqInBlocks(numBlocks) * distance);
+
+}
